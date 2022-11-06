3491f47b9b0de422b3832f34778fb30e1bb4205f
mertovich <88425310+mertovich@users.noreply.github.com>
2022-11-06 14:49:31 +0300

    update: user model construct

--- Models/User.cs
+++ Models/User.cs
@@ -5,13 +5,15 @@ public class User
     public string Id { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
+    public string Email { get; set; }
     public string UserName { get; set; }
     
-    public User(string name, string surname, string userName)
+    public User(string name, string surname,string email, string userName)
     {
         Id = Guid.NewGuid().ToString();
         Name = name;
         Surname = surname;
+        Email = email;
         UserName = userName;
     }
 }
