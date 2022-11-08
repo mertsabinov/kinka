9eb03611bb149c2afe39a45116c3e3328f8a6eba
mertovich <88425310+mertovich@users.noreply.github.com>
2022-11-08 14:02:47 +0300

    add: login controller login control

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 
+using kinka.Data;
 using Microsoft.AspNetCore.Mvc;
 
 public class LoginController : Controller
@@ -6,5 +7,16 @@ public class LoginController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Control(User user)
+    {
+        string error = UserDbManager.UserLogin(user);
+        if (error is not null)
+        {
+            return RedirectToAction("Index");
+        }
+        return Redirect("/");
     }
 }
