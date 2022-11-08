592b93e6a3aa85b85a3d0aa0f4acfc7d3f8dc646
mertovich <88425310+mertovich@users.noreply.github.com>
2022-11-08 14:02:37 +0300

    add: register controller register control

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,8 +1,20 @@
+using kinka.Data;
 using Microsoft.AspNetCore.Mvc;
 
 public class RegisterController: Controller {
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Control(User user)
+    {
+        string error = UserDbManager.UserRegister(user);
+        if (error is not null)
+        {
+            return RedirectToAction("Index");
+        }
+        return Redirect("/");
     }
 }
