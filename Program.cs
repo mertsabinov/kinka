96a946453cb78d2c72e244ca1a7a080d3e1452a0
mertovich <88425310+mertovich@users.noreply.github.com>
2022-11-13 14:07:02 +0200

    add: session

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
