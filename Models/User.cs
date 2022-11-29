aebba187fad7eac3500837c07d330454622ae9a1
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-11-29 12:00:49 +0300

    add: Models user displey name

--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace kinka.Data;
 
 public class User
 {
     public string Id { get; set; }
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string Email { get; set; }
-    public string UserName { get; set; }
     
+    [Display(Name = "your name")]
+    public string Name { get; set; }
+    [Display(Name = "your surname")]
+    public string Surname { get; set; }
+    [Display(Name = "your Email")]
+    public string Email { get; set; }
+    [Display(Name = "your username")]
+    public string UserName { get; set; }
+    [Display(Name = "your password")]
     public string Password { get; set; }
-   
+   [Display(Name = "your user profile image url")]
     public string UserImg { get; set; }
     public User(){}
     public User(string name, string surname,string email, string userName, string password)
