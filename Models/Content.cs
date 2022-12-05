8f49d35aa979bce0fc990770ac61bc4bccde1ecf
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-05 14:08:11 +0300

    add: content new attribute

--- Models/Content.cs
+++ Models/Content.cs
@@ -5,15 +5,18 @@ public class Content
     [Required]
     public string Id { get; set; }
     [Required]
+    public string PostTitle { get; set; }
+    [Required]
     public string Post { get; set; }
     [Required]
     public int LikesCount { get; set; }
     [Required]
     public string OwnerId { get; set; }
 
-    public Content(string post, string ownerId)
+    public Content(string postTitle, string post, string ownerId)
     {
         Id = Guid.NewGuid().ToString();
+        PostTitle = postTitle;
         Post = post;
         LikesCount = 0;
         OwnerId = ownerId;
