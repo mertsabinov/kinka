6c20440769ecfd62e6071d05db5602de2e9d8687
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-05 14:03:12 +0300

    add: user class Required tag

--- Models/User.cs
+++ Models/User.cs
@@ -4,20 +4,26 @@ namespace kinka.Data;
 
 public class User
 {
+    [Required]
     public string Id { get; set; }
-    
     [Display(Name = "your name")]
+    [Required]
     public string Name { get; set; }
     [Display(Name = "your surname")]
+    [Required]
     public string Surname { get; set; }
     [Display(Name = "your Email")]
+    [Required]
     public string Email { get; set; }
     [Display(Name = "your username")]
+    [Required]
     public string UserName { get; set; }
     [Display(Name = "your password")]
+    [Required]
     public string Password { get; set; }
    [Display(Name = "your user profile image url")]
-    public string UserImg { get; set; }
+   [Required] 
+   public string UserImg { get; set; }
     public User(){}
     public User(string name, string surname,string email, string userName, string password)
     {
@@ -28,5 +34,4 @@ public class User
         UserName = userName;
         Password = password;
     }
-
 }
