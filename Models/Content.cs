55c3ad9f3bde91ae2a3edd017904c0e176e2173b
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-05 14:03:20 +0300

    add: content page Required tag

--- Models/Content.cs
+++ Models/Content.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Content
 {
+    [Required]
     public string Id { get; set; }
+    [Required]
     public string Post { get; set; }
+    [Required]
     public int LikesCount { get; set; }
+    [Required]
     public string OwnerId { get; set; }
 
     public Content(string post, string ownerId)
