c53f36b632798c8f40794a017a2a36d7a33e0d45
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-06 14:09:56 +0300

    add: conten db manager save content and get all content method

--- Data/ContentDbManager.cs
+++ Data/ContentDbManager.cs
@@ -1,6 +1,18 @@
 namespace kinka.Data;
 
-public class ContentDbManager
+public static class ContentDbManager
 {
-    
+    private static List<Content> _db = new List<Content>();
+
+    public static List<Content> ContentGetAll()
+    {
+        return _db;
+    }
+
+    public static void ContentSave(Content content)
+    {
+        content.Id = Guid.NewGuid().ToString();
+        content.UsersWhoLike = new List<string>();
+        _db.Add(content);
+    }
 }
